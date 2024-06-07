435f0a6938ca19ac4b99356e54dbbcd184ca5725
goodhamgupta <shubhamg2208@live.com>
2024-06-07 11:45:19 +0800

    Accept http request and give response

--- app/main.py
+++ app/main.py
@@ -9,7 +9,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    while True:
+        client_sock, client_addr = server_socket.accept() # wait for client
+        client_sock.sendall(b"HTTP/1.1 200 OK \r\n\r\n")
+        client_sock.close()
 
 
 if __name__ == "__main__":
