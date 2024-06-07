c268042a84865f231ad8cf4ae4b6d3dd3b69927b
goodhamgupta <shubhamg2208@live.com>
2024-06-07 16:45:55 +0800

    Add support for cli arguments

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 import socket
 from pathlib import Path
 from threading import Thread
+from argparse import Namespace
+import argparse
+from typing import Tuple
 
 
 class View:
@@ -74,7 +77,8 @@ class View:
             bytes: The HTTP response in bytes.
         """
         fname = args["url"].split("/")[2]
-        path = Path(f"/tmp/{fname}")
+        root_dir = getattr(args["parser_args"], "directory")
+        path = Path(f"{root_dir}/{fname}")
         if path.exists():
             content = path.read_text()
             content_len = len(content)
@@ -83,7 +87,7 @@ class View:
             return b"HTTP/1.1 404 Not Found\r\n\r\n"
 
 
-def router(request: str) -> bytes:
+def router(request: str, parser_args: Namespace) -> bytes:
     """
     Checks the requested URL and returns the appropriate HTTP response.
 
@@ -104,10 +108,10 @@ def router(request: str) -> bytes:
     endpoint = "".join(url.split("/")[ENDPOINT_SLICE])
     print(f"Endpoint: {endpoint}")
     mapping = {"": View.root, "echo": View.echo, "user-agent": View.user_agent, "files": View.files}
-    return mapping.get(endpoint, View.not_found)(url=url, request_lines=request_lines)
+    return mapping.get(endpoint, View.not_found)(url=url, request_lines=request_lines, parser_args=parser_args)
 
 
-def process_request(client_sock, client_addr):
+def process_request(client_sock: socket.socket, client_addr: Tuple[str], parser_args: Namespace):
     """
     Processes the incoming client request and sends back the appropriate HTTP response.
 
@@ -123,7 +127,7 @@ def process_request(client_sock, client_addr):
     # User-Agent: curl/8.4.0
     # Accept: */*
     print("Received request:", request)
-    response = router(request)
+    response = router(request, parser_args=parser_args)
     client_sock.sendall(response)
     client_sock.close()
 
@@ -135,11 +139,14 @@ def main():
     The server listens on localhost at port 4221. For each incoming client connection,
     a new thread is spawned to handle the request using the `process_request` function.
     """
+    parser = argparse.ArgumentParser(description="HTTP Server")
+    parser.add_argument("--directory", type=str, help="Directory to serve files from", default="/tmp/")
+    parser_args = parser.parse_args()
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.listen(5)
     while server_socket:
         client_sock, client_addr = server_socket.accept()  # wait for client
-        Thread(target=process_request, args=(client_sock, client_addr)).start()
+        Thread(target=process_request, args=(client_sock, client_addr, parser_args)).start()
 
 
 if __name__ == "__main__":
