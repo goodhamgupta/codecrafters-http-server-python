dc3dbaf4d95a2dbd7f2dd3653292c03f9c41e91b
goodhamgupta <shubhamg2208@live.com>
2024-06-07 17:32:10 +0800

    Add support for content encoding header

--- app/main.py
+++ app/main.py
@@ -7,6 +7,9 @@ from typing import Tuple
 
 
 class View:
+    ENCODING_IDX = 4
+    VALID_ENCODINGS = ["utf-8", "utf-16", "utf-32", "gzip"]
+
     @staticmethod
     def root(**args) -> bytes:
         """
@@ -17,8 +20,8 @@ class View:
         """
         return b"HTTP/1.1 200 OK\r\n\r\n"
 
-    @staticmethod
-    def echo(**args) -> bytes:
+    @classmethod
+    def echo(cls, **args) -> bytes:
         """
         Returns the HTTP response for the echo URL.
 
@@ -29,10 +32,23 @@ class View:
             bytes: The HTTP response in bytes.
         """
         content = args["url"].split("/")[2]
+        content_encoding_header = ""
+        if len(args["request_lines"]) > cls.ENCODING_IDX:
+            encoding = args["request_lines"][cls.ENCODING_IDX].split()[1]
+            if encoding in cls.VALID_ENCODINGS:
+                content_encoding_header = f"Content-Encoding: {encoding}\r\n"
+            else:
+                # Invalid Encoding. Looks the same as the default response, but this is just for demonstration purposes.
+                content_encoding_header = f"Content-Encoding: {encoding}\r\n"
         content_len = len(content)
-        return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_len}\r\n\r\n{content}".encode(
-            "utf-8"
-        )
+        if len(content_encoding_header) > 0:
+            return f"HTTP/1.1 200 OK\r\n{content_encoding_header}\r\nContent-Type: text/plain\r\nContent-Length: {content_len}\r\n\r\n{content}".encode(
+                "utf-8"
+            )
+        else:
+            return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_len}\r\n\r\n{content}".encode(
+                "utf-8"
+            )
 
     @staticmethod
     def not_found(**args) -> bytes:
@@ -79,12 +95,14 @@ class View:
         fname = args["url"].split("/")[2]
         root_dir = getattr(args["parser_args"], "directory")
         path = Path(f"{root_dir}/{fname}")
-        method = args['request_lines'][0].split()[0].strip().upper()
+        method = args["request_lines"][0].split()[0].strip().upper()
         if method == "GET":
             if path.exists():
                 content = path.read_text()
                 content_len = len(content.encode("utf-8"))
-                return f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {content_len}\r\n\r\n{content}".encode("utf-8")
+                return f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {content_len}\r\n\r\n{content}".encode(
+                    "utf-8"
+                )
             else:
                 return b"HTTP/1.1 404 Not Found\r\n\r\n"
         elif method == "POST":
@@ -116,11 +134,20 @@ def router(request: str, parser_args: Namespace) -> bytes:
     print(f"Requested URL: {url}")
     endpoint = "".join(url.split("/")[ENDPOINT_SLICE])
     print(f"Endpoint: {endpoint}")
-    mapping = {"": View.root, "echo": View.echo, "user-agent": View.user_agent, "files": View.files}
-    return mapping.get(endpoint, View.not_found)(url=url, request_lines=request_lines, parser_args=parser_args)
+    mapping = {
+        "": View.root,
+        "echo": View.echo,
+        "user-agent": View.user_agent,
+        "files": View.files,
+    }
+    return mapping.get(endpoint, View.not_found)(
+        url=url, request_lines=request_lines, parser_args=parser_args
+    )
 
 
-def process_request(client_sock: socket.socket, client_addr: Tuple[str], parser_args: Namespace):
+def process_request(
+    client_sock: socket.socket, client_addr: Tuple[str], parser_args: Namespace
+):
     """
     Processes the incoming client request and sends back the appropriate HTTP response.
 
@@ -149,13 +176,17 @@ def main():
     a new thread is spawned to handle the request using the `process_request` function.
     """
     parser = argparse.ArgumentParser(description="HTTP Server")
-    parser.add_argument("--directory", type=str, help="Directory to serve files from", default="/tmp/")
+    parser.add_argument(
+        "--directory", type=str, help="Directory to serve files from", default="/tmp/"
+    )
     parser_args = parser.parse_args()
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.listen(5)
     while server_socket:
         client_sock, client_addr = server_socket.accept()  # wait for client
-        Thread(target=process_request, args=(client_sock, client_addr, parser_args)).start()
+        Thread(
+            target=process_request, args=(client_sock, client_addr, parser_args)
+        ).start()
 
 
 if __name__ == "__main__":
